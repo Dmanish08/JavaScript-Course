3b57bd0cd7f5bed648af7eaeab7de7c2ba1fbdf5
Dmanish08 <debmanish6@gmail.com>
2024-03-03 21:51:00 +0530

    Updated functions

--- 03-basics/01-functions.js
+++ 03-basics/01-functions.js
@@ -2,7 +2,7 @@ function addTwoNumbers(num1, num2){
     return num1 + num2
 }
 
-console.log(addTwoNumbers(3,4));
+// console.log(addTwoNumbers(3,4));
 
 function loginUser(username){
     // if (!username){
@@ -17,6 +17,34 @@ function loginUser(username){
 }
 
 // console.log(loginUser("Chandrachood"));
-console.log(loginUser(""));
+// console.log(loginUser(""));
 
 
+
+
+/************Objects & Arrays in function***************/
+
+function calculateCartPrice(...num){
+    return num
+}
+
+console.log(calculateCartPrice(200, 400, 60000));
+
+const user = {
+    username: "Chandrachood",
+    age: 45
+}
+
+function getDetails(obj){                                               // accessing an object using function
+    return `Username is ${obj.username} and Age is ${obj.age}`
+}
+
+console.log(getDetails(user));
+
+const itemList = ["Shoes", "Bag", "Headphones"]
+
+function getArray(arr){                                                 // accessing an array using function
+    return arr[2]
+}
+
+console.log(getArray(itemList));
